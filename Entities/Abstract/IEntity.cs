03f5401cb838624c9b30fded8e4295b4b181c3d1
Arda Ağdemir <ardagdemirr@gmail.com>
2021-03-09 22:26:02 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Entity' yi implement eden class bir veritabı tablosudur.
+    public interface IEntity
+    {
+    }
+}
