7949ade970e94218719ca4c655b17040a8720a2d
Arda Ağdemir <ardagdemirr@gmail.com>
2021-03-23 19:16:11 +0300

    revised

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace Business.Concrete
 {
-    //Yapı hiç değişmedi, burada entityFrameWork e bir bağlılık söz konusu değil
+    //Yapı hiç değişmedi, burada entityFramework e bir bağlılık söz konusu değil
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
@@ -21,6 +21,16 @@ namespace Business.Concrete
         {
             //Business Code
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p=> p.CategoryId == id); //girilen id categoryId ye eşit ise filtrele
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=> p.UnitPrice>=min && p.UnitPrice<=max);
         }
     }
 }
