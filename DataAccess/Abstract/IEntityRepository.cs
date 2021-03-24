832f75acb856f62eb54cc45eeafe24faf599cdf2
Arda Ağdemir <ardagdemirr@gmail.com>
2021-03-24 22:30:47 +0300

    revised

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -7,10 +7,12 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    //Generic yapısı, T ile bir değşken atamış olduk.
-    public interface IEntityRepository<T> where T:class, IEntity, new() //T değişkeni, yalnızca veritabanı nesnelerini(Entities-concrete) implemente edebilir.
+    //Generic
+    //T değişkeni, yalnızca veritabanı nesnelerini(Entities-concrete) implemente edebilir.
+    public interface IEntityRepository<T> where T:class, IEntity, new() 
     {
-        List<T> GetAll(Expression<Func<T, bool>> filter=null); //Bir listeden bir elemanı seçebilmek için yazılan kod satırıdır. Filtreleme yapmamızı sağlar.
+        //Bir listeden bir elemanı seçebilmek için yazılan kod satırıdır. Filtreleme yapılmasını sağlar.
+        List<T> GetAll(Expression<Func<T, bool>> filter=null); 
         T GetT(Expression<Func<T, bool>> filter = null);
         void Add(T entity);
         void Update(T entity);
